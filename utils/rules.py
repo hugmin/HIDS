173ce76077fa78b60a1c11b98361379bb1858771
hei-sso <lak78998@gmail.com>
2025-06-05 12:43:23 +0900

    Delete utils directory

--- utils/rules.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def load_malicious_hashes(file_path):
-    try:
-        with open(file_path, 'r', encoding='utf-8') as f:
-            return set(json.load(f))  # List -> Set for fast lookup
-    except Exception:
-        return set()
